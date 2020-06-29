8d08b82b9e704ac39493fefeb661ac9b9e14baee
NKazakova <nkazakova@ibs.ru>
2020-06-30 00:27:55 +0300

    Add main dependency injection

--- app/src/main/java/ru/kazakovanet/synoptic/SynopticApplication.kt
+++ app/src/main/java/ru/kazakovanet/synoptic/SynopticApplication.kt
@@ -7,7 +7,12 @@ import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
 import org.kodein.di.generic.singleton
+import ru.kazakovanet.synoptic.data.db.CurrentWeatherDao
 import ru.kazakovanet.synoptic.data.db.SynopticDatabase
+import ru.kazakovanet.synoptic.data.db.SynopticDatabase_Impl
+import ru.kazakovanet.synoptic.data.network.*
+import ru.kazakovanet.synoptic.data.repository.SynopticRepository
+import ru.kazakovanet.synoptic.data.repository.SynopticRepositoryImpl
 
 /**
  * Created by NKazakova on 27.06.2020.
@@ -17,7 +22,11 @@ class SynopticApplication : Application(), KodeinAware {
     override val kodein = Kodein.lazy {
         import(androidXModule(this@SynopticApplication))
 
-        bind() from singleton { SynopticDatabase(this@SynopticApplication) }
-        bind() from singleton { instance<SynopticDatabase>().currentWeatherDao() }
+        bind<SynopticDatabase>() with singleton { SynopticDatabase_Impl() }
+        bind<CurrentWeatherDao>() with singleton { instance<SynopticDatabase>().currentWeatherDao() }
+        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
+        bind() from singleton { WeatherStackApiService(instance()) }
+        bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
+        bind<SynopticRepository>() with singleton { SynopticRepositoryImpl(instance(), instance()) }
     }
 }
